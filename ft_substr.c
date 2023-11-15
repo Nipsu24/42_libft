63ac156f1c47c4baa0a37e72e83871004c8d06dc
Marius Meier <marius.meier24@gmail.com>
2023-11-15 16:22:25 +0200

    updates ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 15:58:44 by mmeier            #+#    #+#             */
-/*   Updated: 2023/11/08 16:25:59 by mmeier           ###   ########.fr       */
+/*   Updated: 2023/11/15 16:21:37 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
 
-	if (len == '\0')
-		return (0);
+	if (start + len > (ft_strlen(s)))
+		len = (ft_strlen(s) - start);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
 	ptr = (char *) malloc ((len + 1) * sizeof(char));
 	if (ptr == 0)
 		return (NULL);
